426aae528d81621a852776080700b717a1caf017
Anka <runner@Mac-1746284687579.local>
2025-05-03 16:05:07 +0000

    Bump to version v1.0.0-withring-20250503-1531 (matrix-rust-sdk/main 3b84b2c5e757860e60961b8361a3b62005e4831c)

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "9ed59001d3ab79eec2d6e5953e32b83adaec27ec8b8cf7f99d44e1e6fab1e63d"
-let version = "v1.0.18-custom-20250503"
+let checksum = "a697fd50b9e318052fc879e85430df11da9f50fa571eda8889f75dfcdbf2bca6"
+let version = "v1.0.0-withring-20250503-1531"
 let url = "https://github.com/hek4ek/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
