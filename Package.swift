53f9e5810bd9c96757425f83a2b9a16555a3b5ea
Viktor Kumanenkov <>
2025-05-03 15:20:32 +0300

    fix Package link

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 let checksum = "9ed59001d3ab79eec2d6e5953e32b83adaec27ec8b8cf7f99d44e1e6fab1e63d"
 let version = "v1.0.18-custom-20250503"
-let url = "https://github.com/matrix-org/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
+let url = "https://github.com/hek4ek/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
     platforms: [
